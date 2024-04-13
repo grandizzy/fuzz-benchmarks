1ea235b7ca07cf572fd261c310b29d90a7c33855
grandizzy <grandizzy.the.egg@gmail.com>
2024-04-13 13:03:55 +0300

    Add simple test to populate dictionary from result

--- test/research/vera_dschief.t.sol
+++ test/research/vera_dschief.t.sol
@@ -4,10 +4,22 @@ pragma solidity 0.8.18;
 import {Test} from "forge-std/Test.sol";
 
 contract SimpleDSChief {
+    bool hacked;
+    event Slate(bytes32 indexed _id);
     mapping(bytes32 => address) public slates;
     mapping(address => bytes32) public votes;
     mapping(address => uint256) public approvals;
     mapping(address => uint256) public deposits;
+
+    modifier checkInvariant() {
+        _;
+
+        bytes32 senderSlate = votes[msg.sender];
+        address option = slates[senderSlate];
+        uint256 senderDeposit = deposits[msg.sender];
+
+        hacked = approvals[option] < senderDeposit;
+    }
 
     function lock(uint256 wad) public {
         deposits[msg.sender] = add(deposits[msg.sender], wad);
@@ -30,15 +42,20 @@ contract SimpleDSChief {
         bytes32 hash = keccak256(abi.encodePacked(yay));
 
         slates[hash] = yay;
+        emit Slate(hash);
 
         return hash;
     }
 
-    function voteSlate(bytes32 slate) public {
+    function voteSlate(bytes32 slate) public checkInvariant {
         uint256 weight = deposits[msg.sender];
         subWeight(weight, votes[msg.sender]);
         votes[msg.sender] = slate;
         addWeight(weight, votes[msg.sender]);
+    }
+
+    function isHacked() public view returns (bool) {
+        return hacked;
     }
 
     function addWeight(uint256 weight, bytes32 slate) internal {
@@ -62,28 +79,14 @@ contract SimpleDSChief {
 
 contract SimpleDSChiefTest is Test {
     SimpleDSChief dsChief;
-    address alice = makeAddr("alice");
-    address bob = makeAddr("bob");
 
     function setUp() public {
         dsChief = new SimpleDSChief();
-
-        targetSender(alice);
-        targetSender(bob);
     }
 
     /// forge-config: default.invariant.runs = 1000
     function invariant_check() public view {
-        assertTrue(getInvariant(alice));
-        assertTrue(getInvariant(bob));
-    }
-
-    function getInvariant(address sender) public view returns (bool) {
-        bytes32 senderSlate = dsChief.votes(sender);
-        address option = dsChief.slates(senderSlate);
-        uint256 senderDeposit = dsChief.deposits(sender);
-
-        return (dsChief.approvals(option) >= senderDeposit);
+        assertFalse(dsChief.isHacked());
     }
 }
 
@@ -115,7 +118,40 @@ contract SimplifiedDSChiefCheckTest is Test {
         dsChief = new SimplifiedDSChief();
     }
 
+    /// forge-config: default.invariant.depth = 1000
     function invariant_check_ever_matched() public view {
         assertFalse(dsChief.wasEverMatched());
+    }
+}
+
+contract SimpleFindFromReturn {
+    bool found = false;
+
+    function seed() public returns (int256) {
+        int256 mystery = 13337;
+        return (1337 + mystery);
+    }
+
+    function find(int256 i) public {
+        int256 mystery = 13337;
+        if (i == 1337 + mystery) {
+            found = true;
+        }
+    }
+
+    function getFound() public view returns (bool) {
+        return found;
+    }
+}
+
+contract SimpleFindFromReturnTest is Test {
+    SimpleFindFromReturn target;
+
+    function setUp() public {
+        target = new SimpleFindFromReturn();
+    }
+
+    function invariant_not_found() public view {
+        assertFalse(target.getFound());
     }
 }
